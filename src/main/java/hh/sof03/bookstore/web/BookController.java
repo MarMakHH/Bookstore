8efa42b076391dec038f6d2a7d911bfe0943b9a6
bhi021@myy.haaga-helia.fi <bhi021@myy.haaga-helia.fi>
2024-09-16 20:34:04 +0300

    book editing in booklist

--- src/main/java/hh/sof03/bookstore/web/BookController.java
+++ src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -43,4 +43,10 @@ public class BookController {
         repository.deleteById(bookId);
         return "redirect:../booklist";
     }
+
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+        model.addAttribute("book", repository.findById(bookId));
+        return "editbook";
+    }
 }
