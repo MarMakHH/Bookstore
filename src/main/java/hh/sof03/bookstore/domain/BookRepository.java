c5260cf2bcb2662c8b686ffd856d2e2306e12680
MarMakHH <bhi021@myy.haaga-helia.fi>
2024-09-17 09:22:13 +0300

    removed useless code

--- src/main/java/hh/sof03/bookstore/domain/BookRepository.java
+++ src/main/java/hh/sof03/bookstore/domain/BookRepository.java
@@ -1,9 +1,9 @@
 package hh.sof03.bookstore.domain;
 
-import java.util.List;
+//import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Long>{
-    List<Book> findAll();
+    //List<Book> findAll();
 }
