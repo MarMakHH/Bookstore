c30683c544e0b1fc216fbe3a45254e68f816a432
bhi021@myy.haaga-helia.fi <bhi021@myy.haaga-helia.fi>
2024-09-23 18:53:04 +0300

    added categories

--- src/main/java/hh/sof03/bookstore/BookstoreApplication.java
+++ src/main/java/hh/sof03/bookstore/BookstoreApplication.java
@@ -1,5 +1,7 @@
 package hh.sof03.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,27 +9,38 @@ import org.springframework.context.annotation.Bean;
 
 import hh.sof03.bookstore.domain.Book;
 import hh.sof03.bookstore.domain.BookRepository;
+import hh.sof03.bookstore.domain.Category;
+import hh.sof03.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
 	@Bean
-	public CommandLineRunner demo(BookRepository repository) {
+	public CommandLineRunner demo(BookRepository bookrepo, CategoryRepository categoryrepo) {
 		return (args) -> {
-			repository.save(
+			categoryrepo.save(new Category("Fiction"));
+			categoryrepo.save(new Category("Textbook"));
+			categoryrepo.save(new Category("Scifi"));
+
+			bookrepo.save(
 					new Book("Hobitti : eli Sinne ja takaisin", "J. R. R. Tolkien", 2024, "9789510468524", 27.90));
-			repository.save(new Book("JAVA-OHJELMOINTI + CD", "Kyppö Jorma", 2008, "9789521413568", 51.00));
-			repository.save(new Book("Spring Boot 3 and Spring Framework 6", "Christian Ullenboom", 2024,
+			bookrepo.save(new Book("JAVA-OHJELMOINTI + CD", "Kyppö Jorma", 2008, "9789521413568", 51.00));
+			bookrepo.save(new Book("Spring Boot 3 and Spring Framework 6", "Christian Ullenboom", 2024,
 					"9781493224753", 55.90));
 
-			System.out.println("Books found with findAll():");
-			System.out.println("-------------------------------");
-			repository.findAll().forEach(book -> {
-				System.out.println(book.toString());
+			log.info("Categories found with findAll()");
+			categoryrepo.findAll().forEach(category -> {
+				log.info(category.toString());
+			});
+
+			log.info("Books found with findAll():");
+			bookrepo.findAll().forEach(book -> {
+				log.info(book.toString());
 			});
 		};
 	}
