bba54b84610e87fce20ffcd8619d96cc7adaed94
bhi021@myy.haaga-helia.fi <bhi021@myy.haaga-helia.fi>
2024-09-30 18:58:14 +0300

    added autogen REST

--- src/main/java/hh/sof03/bookstore/domain/Book.java
+++ src/main/java/hh/sof03/bookstore/domain/Book.java
@@ -1,6 +1,5 @@
 package hh.sof03.bookstore.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Entity;
