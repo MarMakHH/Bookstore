74476fa9265db466f1fb96f0814a4b957eb79fb4
bhi021@myy.haaga-helia.fi <bhi021@myy.haaga-helia.fi>
2024-10-08 00:17:51 +0300

    changed in-memory users to db users

--- src/main/java/hh/sof03/bookstore/BookstoreApplication.java
+++ src/main/java/hh/sof03/bookstore/BookstoreApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import hh.sof03.bookstore.domain.AppUser;
+import hh.sof03.bookstore.domain.AppUserRepository;
 import hh.sof03.bookstore.domain.Book;
 import hh.sof03.bookstore.domain.BookRepository;
 import hh.sof03.bookstore.domain.Category;
@@ -21,7 +23,8 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(BookRepository bookrepo, CategoryRepository categoryrepo) {
+	public CommandLineRunner demo(BookRepository bookrepo, CategoryRepository categoryrepo,
+			AppUserRepository userRepo) {
 		return (args) -> {
 			Category c1 = new Category("Fiction");
 			categoryrepo.save(c1);
@@ -36,6 +39,14 @@ public class BookstoreApplication {
 			bookrepo.save(new Book("Spring Boot 3 and Spring Framework 6", "Christian Ullenboom", 2024,
 					"9781493224753", 55.90, c2));
 
+			AppUser user1 = new AppUser("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "",
+					"USER");
+			AppUser user2 = new AppUser("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "",
+					"ADMIN");
+
+			userRepo.save(user1);
+			userRepo.save(user2);
+			
 			log.info("Categories found with findAll()");
 			categoryrepo.findAll().forEach(category -> {
 				log.info(category.toString());
